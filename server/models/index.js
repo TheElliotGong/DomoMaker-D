c3e61679b245ab1f0270598fd7d006f7ac186428
thetyper101 <ewcgong@hotmail.com>
2023-11-14 18:31:09 -0500

    finalized back to domomaker e

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,2 @@
 module.exports.Account = require('./Account.js');
 module.exports.Domo = require('./Domo.js');
-module.exports.Profile = require('./Profile.js');
-module.exports.Video = require('./Video.js');
